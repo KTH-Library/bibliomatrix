47b75d828bf29eada7c3bfb5f0cd34164d463ac0
Markus Skyttner <markussk@kth.se>
2019-10-28 17:38:48 +0100

    add function to return data related to organizational units

--- R/data_bibmon.R
+++ R/data_bibmon.R
@@ -1,31 +1,47 @@
-#' Retrieve data for the first ABM table
+#' Connection to Bibliometrics data source for KTH
 #' 
-#' @return tibble with data for organizational units
+#' This connection relies on an .Renviron file with environment variables.
+#' 
+#' Make sure one exists and that variables are set for:
+#' DBHOST, DBNAME, DBUSER, DBPASS
+#' 
+#' @return database connection
+#' @import DBI odbc
 #' @export
-abm_tab1 <- function() 
-{
-  
-  library(DBI)
-  library(dplyr)
-  library(tidyr)
-  
-  con_bibmon <- dbConnect(
+con_bib <- function() {
+  con <- dbConnect(
     odbc::odbc(), driver = "ODBC Driver 17 for SQL Server", 
     server = Sys.getenv("DBHOST"), database = Sys.getenv("DBNAME"), Port = 1433, 
     UID = Sys.getenv("DBUSER"), PWD = Sys.getenv("DBPASS")
   )
+}
+  
+#' Retrieve data for the first ABM table
+#' 
+#' @return tibble with data for organizational units
+#' @export
+#' @import DBI dplyr tidyr purrr
+#' 
+abm_tab1 <- function(con = con_bib()) 
+{
   
   df_school <- 
-    tbl(con_bibmon, "t_diva") %>%
+    tbl(con, "t_diva") %>%
+    # discard years outside of the 2012-2018 interval
     filter(Doc_Year > 2011, Doc_Year < 2019) %>%
+    # group by organizational unit, publication type and years
     group_by(unit_long, Publication_Type_DiVA, Doc_Year, pt_ordning) %>%
+    # calculate total and weigths
     summarise(
       w_d_Sum = sum(w_unit),
       wos_coverage = sum(wos_coverage * w_unit)
     ) %>%
+    # arrange by organizational unit, year and publication sort order
     arrange(unit_long, Doc_Year, pt_ordning) %>%
+    # fetch the data from the connection
     collect()
   
+  # NB: The root level KTH is considered to be at "school" level
   df_school2 <- bind_rows(
     df_school,
     df_school %>%
@@ -35,17 +51,28 @@ abm_tab1 <- function()
         wos_coverage = sum(wos_coverage) / sum(w_d_Sum)
       ) %>%
       mutate(Doc_Year = NA)
-  )
+    )  %>%
+    mutate(unit_abbrev = if_else(
+      unit_long == "KTH Royal Institute of Technology",
+      "KTH",
+      extract_re(unit_long)) 
+    ) %>%
+    left_join(abm_units(), by = "unit_abbrev")
+  
   
   df_dep <- 
-    tbl(con_bibmon, "PUBL_DEPT_FRAC") %>% 
+    tbl(con, "PUBL_DEPT_FRAC") %>% 
     filter(Doc_Year > 2011, Doc_Year < 2019) %>%
     collect() %>%
     select(-starts_with("_")) %>%
-    rename(unit_id = unit, wos_coverage = wos_coverage_Mean)
+    rename(unit_id = unit, wos_coverage = wos_coverage_Mean) %>%
+    mutate(unit_id = as.numeric(unit_id)) %>%
+    left_join(abm_units()) #, by = "unit_id")
   
-  df <- 
+  df <-  
+    # combine the two previous results
     df_school2 %>% bind_rows(df_dep) %>%
+    # pivot to get yearly summary values horizontally as columns
     pivot_wider(names_from = Doc_Year, values_from = c(wos_coverage, w_d_Sum)) %>%
     ungroup()
 
@@ -55,33 +82,127 @@ abm_tab1 <- function()
     distinct(pt_ordning, Publication_Type_DiVA)
   
   res <-
+    # add publication type sort order
     df %>% left_join(lookup_pt, by = c("Publication_Type_DiVA")) %>%
-    # 
-    rename(sort_order = pt_ordning.y) %>%
+    # clarify that this is sort order for publications
+    rename(pubtype_order = pt_ordning.y) %>%
     # harmonize field names with existing abm report format
     rename(
       `Publication Type` = Publication_Type_DiVA,
       `WoS Coverage` = wos_coverage_NA,
       Total = w_d_Sum_NA
     ) %>%
-    select(-c(starts_with("wos_coverage"), pt_ordning.x))
+    select(-c(starts_with("wos_coverage"), pt_ordning.x)) 
   
-  # TODO:
-  # rename på alla w_d_Sum_\\(d{4}) -> $1
+  res <- 
+    set_names(res, replace_if_re(names(res))) %>%
+    mutate(`Organizational Unit` = if_else(
+      is.na(unit_long_eng), 
+      unit_long_swe,
+      unit_long_eng)
+    ) %>%
+    select(`Organizational Unit`, everything()) %>%
+    rename("id" = "unit_id") %>%
+    select(-c(starts_with("unit_")))
   
-  # TODO: koppling mellan unit_id och unit_long
-  # unit_long för institution
-  # unit_id för KTH + skolor 
-    
-  dbDisconnect(con_bibmon)
+  
+  dbDisconnect(con)
   
   return (res)
 }
 
-# TODO: fcn to return unit_id, unit_long
 
-# library(readr)
-# 
-# library(purrr)
-# read_rds("~/repos/KTH-Library.github.io/abm/pub.rda") %>%
-#   pluck(1, 1)
+#' Extract regex capture groups
+#' @param x vector of strings
+#' @param re regexp where the first capture group gets extracted
+#' @return vector with string contents of first capture group
+#' @importFrom dplyr nth
+#' @importFrom stringr str_match_all
+#' @importFrom purrr map_chr
+extract_re <- function(x, re) 
+{
+  if (missing(re))
+    re <- "\\(([[:upper:]]{1})([[:upper:]]{1,4})\\)"
+  map_chr(str_match_all(x, re), function(x) nth(x, 2))
+}
+
+replace_if_re <- function(x, 
+  re_if = "^w_d_Sum", 
+  re_replace = "_(\\d{4})$") 
+{
+  sar <- function(x)
+    if_else(grepl(re_if, x), extract_re(x, re_replace), x)
+  
+  map_chr(x, sar)
+}
+
+
+#' Organizational units at KTH
+#' 
+#' @return tibble
+#' @export
+#' @import DBI dplyr tidyr	
+
+abm_units <- function(con = con_bib()) {
+  
+  diva <- 
+    con %>% tbl("DIVA_School_dept") %>%
+    distinct(
+      skola_namn, 
+      Diva_org_id, 
+      Diva_school_id, 
+      Diva_dep_id, 
+      dep_name) %>%
+    collect()
+  
+  lookup <- bind_rows(
+    
+    # root level 1: KTH (hardcoding for now bec cannot find in BIBMON)
+    tibble(
+      unit_long_swe = "KTH Royal Institute of Technology",
+      unit_id = 177,
+      unit_level = 1,
+      unit_abbrev = "KTH"
+    ),
+
+    # level 2: schools
+    diva %>% 
+    filter(!is.na(skola_namn)) %>% 
+    distinct(Diva_school_id, skola_namn) %>%
+    mutate(unit_level = 2) %>%
+    rename(unit_long_swe = skola_namn, unit_id = Diva_school_id) %>%
+    mutate(
+      unit_id = as.numeric(unit_id), 
+      unit_pid = 177,
+      unit_abbrev = extract_re(unit_long_swe)
+    ),
+
+    # level 3: departments
+    diva %>%
+    filter(!is.na(dep_name)) %>%
+    rename(unit_long_swe = dep_name, unit_id = Diva_dep_id) %>% 
+    mutate(
+      unit_level = 3,
+      unit_id = as.numeric(unit_id),
+      unit_pid = as.numeric(Diva_school_id)
+    ) %>%
+    collect()
+  )
+  
+  lookup_abbrev <- 
+    con %>% 
+    tbl("t_diva") %>% 
+    distinct(unit, unit_long, unit_sort) %>%
+    rename(unit_abbrev = unit, unit_long_eng = unit_long) %>%
+    collect()
+
+  res <- 
+    lookup %>%
+    left_join(lookup_abbrev, by = "unit_abbrev") %>% 
+    select(starts_with("unit"))
+  
+  dbDisconnect(con)
+  
+  return (res)
+  
+}
--- README.md
+++ README.md
@@ -35,15 +35,13 @@ library(dplyr)
 # output to a specific set of columns
 
 abm_tab1() %>% 
-filter(unit_long == "KTH Royal Institute of Technology") %>%
-select(unit_long, `Publication Type`, `WoS Coverage`) %>%
+# only show data given a specific unit id (KTH = 177)
+filter(id == 177) %>%
+select(`Organizational Unit`, `Publication Type`, `WoS Coverage`) %>%
 knitr::kable()
-#> Warning: Missing values are always removed in SQL.
-#> Use `SUM(x, na.rm = TRUE)` to silence this warning
-#> This warning is displayed only once per session.
 ```
 
-| unit\_long                        | Publication Type              | WoS Coverage |
+| Organizational Unit               | Publication Type              | WoS Coverage |
 | :-------------------------------- | :---------------------------- | -----------: |
 | KTH Royal Institute of Technology | Article, peer review          |    0.8623932 |
 | KTH Royal Institute of Technology | Article, other                |    0.8080809 |
@@ -58,3 +56,55 @@ knitr::kable()
 | KTH Royal Institute of Technology | Doctorate thesis              |    0.0000000 |
 | KTH Royal Institute of Technology | Licentiate thesis             |    0.0000000 |
 | KTH Royal Institute of Technology | Patent, approved              |    0.0000000 |
+
+This is a basic example of how to retrieve data for the organizational
+units used in the Annual Bibliometric Monitoring:
+
+``` r
+# get data for organizational units at 
+# level 1 (KTH), level 2 (Schools) and level 3 (Departments / Institutions)
+# NB: this table is not yet complete and some entries lack english translations
+
+org <- abm_units()
+
+# inspect some organizational unit ids at "level 2" ie at the school level
+
+org %>%
+filter(unit_level == 2) %>%
+knitr::kable()
+```
+
+| unit\_long\_swe                                    | unit\_id | unit\_level | unit\_abbrev | unit\_pid | unit\_long\_eng                                                             | unit\_sort |
+| :------------------------------------------------- | -------: | ----------: | :----------- | --------: | :-------------------------------------------------------------------------- | ---------: |
+| Skolan för kemi, bioteknologi och hälsa (CBH)      |   879224 |           2 | C            |       177 | School of Engineering Sciences in Chemistry, Biotechnology and Health (CBH) |          2 |
+| Skolan för arkitektur och samhällsbyggnad (ABE)    |     5850 |           2 | A            |       177 | School of Architecture and the Built Environment (ABE)                      |          1 |
+| Skolan för elektroteknik och datavetenskap (EECS)  |   879223 |           2 | E            |       177 | School of Electrical Engineering and Computer Science (EECS)                |          3 |
+| Skolan för industriell teknik och management (ITM) |     6023 |           2 | I            |       177 | School of Industrial Engineering and Management (ITM)                       |          4 |
+| Skolan för teknikvetenskap (SCI)                   |     6091 |           2 | S            |       177 | School of Engineering Sciences (SCI)                                        |          5 |
+
+Some data relating to a specific school using the organizational unit
+id:
+
+``` r
+
+abm_tab1() %>%
+filter(id == 879223) %>%
+select(-id, -pubtype_order, -c(starts_with("20"))) %>%
+knitr::kable()
+```
+
+| Organizational Unit                                          | Publication Type              | WoS Coverage |       Total |
+| :----------------------------------------------------------- | :---------------------------- | -----------: | ----------: |
+| School of Electrical Engineering and Computer Science (EECS) | Article, peer review          |    0.8804322 | 1973.472157 |
+| School of Electrical Engineering and Computer Science (EECS) | Article, other                |    0.7437743 |   79.817100 |
+| School of Electrical Engineering and Computer Science (EECS) | Conference paper, peer review |    0.5764869 | 2876.217229 |
+| School of Electrical Engineering and Computer Science (EECS) | Conference paper, other       |    0.0556550 |  338.736767 |
+| School of Electrical Engineering and Computer Science (EECS) | Book                          |    0.1589574 |   13.525637 |
+| School of Electrical Engineering and Computer Science (EECS) | Anthology (editor)            |    0.0000000 |    5.116667 |
+| School of Electrical Engineering and Computer Science (EECS) | Chapter in book               |    0.0674281 |  121.116739 |
+| School of Electrical Engineering and Computer Science (EECS) | Article, book review          |    0.2045455 |    2.933333 |
+| School of Electrical Engineering and Computer Science (EECS) | Proceeding (editor)           |    0.0000000 |   11.008333 |
+| School of Electrical Engineering and Computer Science (EECS) | Report                        |    0.0000000 |   91.609695 |
+| School of Electrical Engineering and Computer Science (EECS) | Doctorate thesis              |    0.0000000 |  508.000000 |
+| School of Electrical Engineering and Computer Science (EECS) | Licentiate thesis             |    0.0000000 |  167.000000 |
+| School of Electrical Engineering and Computer Science (EECS) | Patent, approved              |    0.0000000 |   17.326190 |

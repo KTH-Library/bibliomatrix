b4a523c2dc5032a837528933ab8d8cdd17180cce
Agne <agnel@kth.se>
2019-11-06 17:07:50 +0100

    Inital versions of functions to retrieve ABM 1.0 tables 1-5.

--- R/abm.R
+++ R/abm.R
@@ -17,7 +17,7 @@ abm_data <- function(con = con_bib(), unit_code, pub_year, unit_level) {
   if (!missing(unit_level))
     res <- res %>% filter(level %in% unit_level)
   
-  return(res)
+  res
 }
 
 #' Retrieve Table 1 (Publications in DiVA) for ABM
@@ -25,7 +25,7 @@ abm_data <- function(con = con_bib(), unit_code, pub_year, unit_level) {
 #' @param con connection to db, default is to use mssql connection
 #' @param unit_code the code for the analyzed unit (KTH, a one letter school code, an integer department code or a KTH-id)
 #' @param pub_year publication year(s) to analyze (optional, assuming master table holds only relevant years)
-#' @return table over publications by type and year for display in ABM
+#' @return data frame with publications by type and year
 #' @import DBI dplyr tidyr purrr
 #' @export
 
@@ -56,29 +56,26 @@ abm_table1 <- function(con = con_bib(), unit_code, pub_year){
     orgdata %>%
     group_by(Publication_Type_DiVA) %>%
     summarise(P_frac = sum(Unit_Fraction),
-              WoS_coverage = sum(Unit_Fraction * !is.na(WebofScience_ID)) / sum(Unit_Fraction) ) %>%
+              WoS_coverage = weighted.mean(!is.na(WebofScience_ID), Unit_Fraction, na.rm = T)) %>%
     ungroup()
-  
+
   dbDisconnect(con)
   
   table1 %>% merge(table2)
 }
-
-table(b$Publication_Type_WoS)
 
 #' Retrieve Table 2 (Citations 3-year window) for ABM
 #' 
 #' @param con connection to db, default is to use mssql connection
 #' @param unit_code the code for the analyzed unit (KTH, a one letter school code, an integer department code or a KTH-id)
 #' @param pub_year publication year(s) to analyze (optional, assuming master table holds only relevant years)
-#' @return table over publications by type and year for display in ABM
+#' @return tibble with citations statistics by year and total
 #' @import DBI dplyr tidyr purrr
 #' @export
 
-abm_table2 <- function(con = con_bib("sqlite"), unit_code, pub_year){
+abm_table2 <- function(con = con_bib(), unit_code, pub_year){
   # Todo:
   # - See abm_table1 above
-  # - Fix strange error related to database connection
 
   # Get publication level data for selected unit (and filter on pub_year if given), relevant WoS doctypes only
   orgdata <- abm_data(unit_code = unit_code) %>%
@@ -86,7 +83,6 @@ abm_table2 <- function(con = con_bib("sqlite"), unit_code, pub_year){
            Publication_Year < max(Publication_Year) - 1) %>%
     mutate(Publication_Year = as.character(Publication_Year)) %>%
     collect()
-
   if(!missing(pub_year))
     orgdata <- filter(orgdata, Publication_Year %in% pub_year)
 
@@ -96,7 +92,7 @@ abm_table2 <- function(con = con_bib("sqlite"), unit_code, pub_year){
     group_by(Publication_Year) %>%
     summarise(P_frac = sum(Unit_Fraction),
               C3_frac = sum(Unit_Fraction * Citations_3yr, na.rm = T),
-              C3 = sum(Unit_Fraction * Citations_3yr, na.rm = T) / sum(Unit_Fraction, na.rm = T)) %>%
+              C3 = weighted.mean(Citations_3yr, Unit_Fraction, na.rm = T)) %>%
     ungroup()
   
   # Summary part of table
@@ -104,8 +100,179 @@ abm_table2 <- function(con = con_bib("sqlite"), unit_code, pub_year){
     orgdata %>%
     summarise(P_frac = sum(Unit_Fraction),
               C3_frac = sum(Unit_Fraction * Citations_3yr, na.rm = T),
-              C3 = sum(Unit_Fraction * Citations_3yr, na.rm = T) / sum(Unit_Fraction, na.rm = T)) %>%
+              C3 = weighted.mean(Citations_3yr, Unit_Fraction, na.rm = T)) %>%
     mutate(Publication_Year = "Total")
+
+  dbDisconnect(con)
 
   rbind(table1, table2)
 }
+
+#' Create integer intervals useful for e.g. sliding means
+#'
+#' @param first the smallest integer in the range to use
+#' @param last the largest integer in the range to use
+#' @param width the desired width of intervals
+#' @return data frame with label for each interval and one row for each year
+
+sliding_intervals <- function(first, last, width){
+  
+  starts <- seq(first, last - width + 1)
+  interval <- paste0(starts, "-", starts + width - 1)
+  
+  data.frame(interval = rep(interval, each = width), x = rep(starts, each = width) + rep(seq(0, width - 1), length(starts)))
+}
+
+#' Retrieve Table 3 (Field normalized citations) for ABM
+#' 
+#' @param con connection to db, default is to use mssql connection
+#' @param unit_code the code for the analyzed unit (KTH, a one letter school code, an integer department code or a KTH-id)
+#' @param pub_year publication year(s) to analyze (optional, assuming master table holds only relevant years)
+#' @return tibble with field normalized citations and number/share of top10 publications by 3 year interval
+#' @import DBI dplyr tidyr purrr
+#' @export
+
+abm_table3 <- function(con = con_bib(), unit_code, pub_year){
+  # Todo:
+  # - See abm_table1 above
+  
+  # Get publication level data for selected unit (and filter on pub_year if given), relevant WoS doctypes only
+  orgdata <- abm_data(unit_code = unit_code) %>%
+    filter(Publication_Type_WoS %in% c("Article", "Review") &
+             Publication_Year < max(Publication_Year)) %>%
+    collect()
+  if(!missing(pub_year))
+    orgdata <- filter(orgdata, Publication_Year %in% pub_year)
+  
+  # Duplicate rows so that publications are connected to all intervals they should belong to according to publication year
+  orgdata3year <- merge(x = orgdata,
+                        y = sliding_intervals(min(orgdata$Publication_Year), max(orgdata$Publication_Year), 3),
+                        by.x = "Publication_Year",
+                        by.y = "x")
+
+  # Year dependent part of table
+  table1 <-
+    orgdata3year %>%
+    group_by(interval) %>%
+    summarise(P_frac = sum(Unit_Fraction),
+              cf = weighted.mean(cf, Unit_Fraction, na.rm = T),
+              top10_count = sum(Ptop10*Unit_Fraction, na.rm = T),
+              top10_share = weighted.mean(Ptop10, Unit_Fraction, na.rm = T)) %>%
+    ungroup()
+  
+  # Summary part of table
+  table2 <-
+    orgdata %>%
+    summarise(P_frac = sum(Unit_Fraction),
+              cf = weighted.mean(cf, Unit_Fraction, na.rm = T),
+              top10_count = sum(Ptop10*Unit_Fraction, na.rm = T),
+              top10_share = weighted.mean(Ptop10, Unit_Fraction, na.rm = T)) %>%
+    mutate(interval = "Total")
+
+  dbDisconnect(con)
+  
+  rbind(table1, table2)
+}
+
+#' Retrieve Table 4 (Journal impact) for ABM
+#' 
+#' @param con connection to db, default is to use mssql connection
+#' @param unit_code the code for the analyzed unit (KTH, a one letter school code, an integer department code or a KTH-id)
+#' @param pub_year publication year(s) to analyze (optional, assuming master table holds only relevant years)
+#' @return tibble with field normalized journal citation score and number/share of publications in top20 journals
+#' @import DBI dplyr tidyr purrr
+#' @export
+
+abm_table4 <- function(con = con_bib(), unit_code, pub_year){
+  # Todo:
+  # - See abm_table1 above
+  
+  # Get publication level data for selected unit (and filter on pub_year if given), relevant WoS doctypes only
+  orgdata <- abm_data(unit_code = unit_code) %>%
+    filter(Publication_Type_WoS %in% c("Article", "Review")) %>%
+    collect()
+  if(!missing(pub_year))
+    orgdata <- filter(orgdata, Publication_Year %in% pub_year)
+  
+  # Duplicate rows so that publications are connected to all intervals they should belong to according to publication year
+  orgdata3year <- merge(x = orgdata,
+                        y = sliding_intervals(min(orgdata$Publication_Year), max(orgdata$Publication_Year), 3),
+                        by.x = "Publication_Year",
+                        by.y = "x")
+  
+  # Year dependent part of table
+  table1 <-
+    orgdata3year %>%
+    group_by(interval) %>%
+    summarise(P_frac = sum(Unit_Fraction),
+              jcf = weighted.mean(jcf, Unit_Fraction, na.rm = T),
+              top10_count = sum(Jtop20*Unit_Fraction, na.rm = T),
+              top10_share = weighted.mean(Jtop20, Unit_Fraction, na.rm = T)) %>%
+    ungroup()
+  
+  # Summary part of table
+  table2 <-
+    orgdata %>%
+    summarise(P_frac = sum(Unit_Fraction),
+              jcf = weighted.mean(jcf, Unit_Fraction, na.rm = T),
+              top10_count = sum(Jtop20*Unit_Fraction, na.rm = T),
+              top10_share = weighted.mean(Jtop20, Unit_Fraction, na.rm = T)) %>%
+    mutate(interval = "Total")
+  
+  dbDisconnect(con)
+  
+  rbind(table1, table2)
+}
+
+#' Retrieve Table 5 (Co-publishing) for ABM
+#' 
+#' @param con connection to db, default is to use mssql connection
+#' @param unit_code the code for the analyzed unit (KTH, a one letter school code, an integer department code or a KTH-id)
+#' @param pub_year publication year(s) to analyze (optional, assuming master table holds only relevant years)
+#' @return tibble with number/share of international copublications and copublications with Swedish non-university organizations
+#' @import DBI dplyr tidyr purrr
+#' @export
+
+abm_table5 <- function(con = con_bib(), unit_code, pub_year){
+  # Todo:
+  # - See abm_table1 above
+  
+  # Get publication level data for selected unit (and filter on pub_year if given), relevant WoS doctypes only
+  orgdata <- abm_data(unit_code = unit_code) %>%
+    filter(Publication_Type_WoS %in% c("Article", "Review")) %>%
+    collect()
+  if(!missing(pub_year))
+    orgdata <- filter(orgdata, Publication_Year %in% pub_year)
+
+  # Duplicate rows so that publications are connected to all intervals they should belong to according to publication year
+  orgdata3year <- merge(x = orgdata,
+                        y = sliding_intervals(min(orgdata$Publication_Year), max(orgdata$Publication_Year), 3),
+                        by.x = "Publication_Year",
+                        by.y = "x")
+  
+  # Year dependent part of table
+  table1 <-
+    orgdata3year %>%
+    group_by(interval) %>%
+    summarise(P_full = n(),
+              nonuniv_count = sum(swe_nuniv, na.rm = T),
+              nonuniv_share = mean(swe_nuniv, na.rm = T),
+              int_count = sum(int, na.rm = T),
+              int_share = mean(int, na.rm = T)) %>%
+    ungroup()
+  
+  # Summary part of table
+  table2 <-
+    orgdata %>%
+    summarise(P_full = n(),
+              nonuniv_count = sum(swe_nuniv, na.rm = T),
+              nonuniv_share = mean(swe_nuniv, na.rm = T),
+              int_count = sum(int, na.rm = T),
+              int_share = mean(int, na.rm = T)) %>%
+    mutate(interval = "Total")
+  
+  dbDisconnect(con)
+  
+  rbind(table1, table2)
+}
+

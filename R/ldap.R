fc555150febdfec8e384f73b995a983c73f574f2
Markus Skyttner <mskyttner@pm.me>
2019-11-07 10:53:18 +0100

    fix #1

--- R/ldap.R
+++ R/ldap.R
@@ -69,16 +69,22 @@ ad_search_accountname <- function(accountname, cfg) {
   ldap_search(query, cfg)
 }
 
-#' Search Active Directory at KTH using ldapsearch
+#' Search Active Directory at KTH using kthid or username
 #'
-#' This function uses the ldapsearch utility to query the KTH Active Directory
+#' This function uses curl to query the KTH Active Directory. It requires 
+#' environment variables to be set in .Renviron, specifically
+#' LDAP_USER, LDAP_PASS, LDAP_HOST and LDAP_BASE for the service 
+#' account used for the queries.
 #' 
-#' Besides the openldap utils used for LDAP queries it also requires environment variables to be set in .Renviron (LDAP_USER, LDAP_PASS, LDAP_HOST and LDAP_BASE for the service account used for the queries)
+#' @details
+#' 
+#' Under the hood, this function issues a curl request similar to this one:
+#' curl --ntlm -user $CREDS \\
+#' ldaps://ldap.foo.com/DC=ads,DC=foo,DC=com?memberOf?sub?(&(sAMAccountName=$USER)(memberOf=CN=$GROUP,OU=Distribution,OU=Groups,DC=ads,DC=foo,DC=com))"
 #'
 #' @param search_term the KTH id or account name to use in the search query
 #' @param search_type one of "kthid" or "accountname" for the type of search to make
 #' @return tibble with key value pairs
-#' @details this function requires that the system library openldap is available ("sudo apt install openldap-utils")
 #' @examples ad_search("markussk", "accountname")
 #' @export
 ad_search <- function(search_term, 
@@ -86,11 +92,8 @@ ad_search <- function(search_term,
 {
   # load the LDAP service account credentials
   cfg <- ldap_config()
-  
   type <- match.arg(search_type)
-  switch(type,
-   kthid = ad_search_kthid(search_term, cfg),
-   accountname = ad_search_accountname(search_term, cfg))
+  ldap_search_curl(search_term, type, cfg)
 }
 
 #' List member of properties from LDAP search
@@ -105,3 +108,54 @@ ad_memberof <- function(search_results) {
   mutate(cn = stringr::str_match(value, "^CN=(.*?),(.*?)$")[, 2]) %>%
   select(memberof = cn)
 }
+
+ldap_cmd_curl <- function(ldap_filter, ldap_host, ldap_base,
+  ldap_attributes = "", ldap_scope = "sub") {
+  # return url pattern like ...
+  # ldap[s]://hostname:port/base_dn?attributes?scope?filter
+  sprintf("%s/%s?%s?%s?%s", ldap_host, ldap_base, ldap_attributes, ldap_scope, ldap_filter)
+}
+
+#' @import curl
+#' @noRd
+ldap_search_curl <- function(term,
+  ldap_search_type = c("accountname", "kthid"),
+  cfg = ldap_config()) {
+  
+  # determine LDAP filter to use based on search type
+  type <- match.arg(ldap_search_type)
+  filter <- switch(type,
+    accountname = sprintf("(sAMAccountName=%s)", term),
+    kthid = sprintf("(ugKthid=%s)", term),
+    stop("ldap search type needs to be one of kthid or accountname")
+  )
+  
+  # make curl request
+  query <- ldap_cmd_curl(filter, cfg$ldap_host, cfg$ldap_base)
+  userpwd <- paste0(cfg$ldap_user, ":", cfg$ldap_pass)
+  handle <- curl::new_handle()
+  curl::handle_setopt(handle, userpwd = userpwd)
+  res <- curl_fetch_memory(query, handle)
+
+  # parse the response  
+  ldif <- 
+    rawToChar(res$content) %>%
+    str_replace_all("\n{2}", "\n") %>% 
+    str_replace_all("\t", "") %>% 
+    trimws() %>% 
+    strsplit(fixed = TRUE, split = "\n") %>%
+    unlist()
+  
+  parse_ldif <- function(ldif) {
+    re <- "^(.*?):\\s{1}(.*?)$"
+    out <- grep(re, ldif, value = TRUE, perl = TRUE)
+    key <- stringr::str_match(out, re)[ ,2]
+    value <- stringr::str_match(out, re)[ ,3]
+    tibble::tibble(key, value)
+  }
+  
+  # return a tibble with results
+  parse_ldif(ldif)
+}
+
+

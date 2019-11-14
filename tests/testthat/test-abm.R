da699de2f7f578cc513808afac4a4d0af681ddf5
tobias <tjep@kth.se>
2019-11-14 14:07:37 +0100

    Initial functional test of result table, for single unit. Passes test_that with OK

--- tests/testthat/test-abm.R
+++ tests/testthat/test-abm.R
@@ -2,22 +2,27 @@
 #
 # Test for git account.
 
-abm_ref <- function(con = con_bib(), table = "cf_glid_dept_frac_aggr", unit_code) {
-  res <- con %>% tbl(table)
-  if (!missing(unit_code))
-    res <- res %>% filter(unit %in% unit_code)
+
+test_that("cf-test", {
   
-  res
-}
+  abm_ref <- function(con = con_bib(), table = "cf_glid_dept_frac_aggr", unit_code) {
+    res <- con %>% tbl(table)
+    if (!missing(unit_code))
+      res <- res %>% filter(unit %in% unit_code)
+    
+    res
+  }
+  
+  testcode<- "5851" # slection of unit to check for manual testing
+  cols<- c("cf_label_year", "w_d_Sum", "cf_scxwo_Mean", "top10_scxwo_Sum", "top10_scxwo_Mean") #, "unit")
+  ref<- abm_ref(table = "cf_glid_dept_frac_aggr", unit_code=testcode)  %>% select(cols)
+  calc<- abm_table3(unit_code=testcode)
+  #colnames(calc)<- cols # not needed with all(X == Y) 
 
-ref<- abm_ref(table = "cf_glid_dept_frac_aggr", unit_code="5851")
+  ref_df<- as.data.frame(ref)
 
-calc<- abm_table3(unit_code="5851")
-cols<- c("unit", "cf_label_year", "w_d_sum", "cf_scxwo_Mean", "top10_scxwo_sum", "top10_scxwo_Mean")
-ref<- ref[,cols]
-
-
-
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+  #testresult<- all_equal(ref_df,calc)
+  expect_true(all(ref_df == calc))
+  #expect_true(testresult)
+ # expect_equal(2 * 2, 4)
 })

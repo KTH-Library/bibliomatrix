0f5e1e3db3addfbe2fa1041cd317cdd9de6458fa
tobias <tjep@kth.se>
2019-11-14 23:00:14 +0100

    Cleanup of code

--- tests/testthat/test-abm.R
+++ tests/testthat/test-abm.R
@@ -2,9 +2,9 @@
 #
 # 
 
-testunit<<- "5851"  # "5851" 13604 5857
-testlevel<<- "dept" #"dept"
-acc_tolerance<- 0.005
+testunit<<- "13604"  # "5851" 13604 5857
+testlevel<<- "dept" #"dept" school
+acc_tolerance<- 0.00001
 
 # Function to read reference data exported from previous ABM
 #----------------------------
@@ -41,6 +41,33 @@ test_that("cf-test", {
   
 })
 
+
+# Test comparing c3-table
+#----------------------------------
+test_that("c3-test", {
+  
+  cols<- c("publication_year", "p_frac_Sum", "c3_frac_Sum", "c3_frac_mean_Sum")
+  refname= paste0("c3_",testlevel,"_frac_aggr")
+  
+  comp_result<- compare_ref(cols=cols, refname=refname, calctable=abm_table2)
+  expect_true(comp_result)
+  
+})
+
+
+# Test comparing jcf-table
+#----------------------------------
+test_that("jcf-test", {
+  
+  cols<- c("jrv_label_year", "Pfrac_Sum", "jrv_mean_frac_Sum", "top20_sum_frac_Sum", "top20_mean_frac_Sum")
+  refname= paste0("jcf_glid_",testlevel,"_frac_aggr")
+  
+  comp_result<- compare_ref(cols=cols, refname=refname, calctable=abm_table4)
+  expect_true(comp_result)
+  
+})
+
+# test_that("cf-test", {
 #   cols<- c("cf_label_year", "w_d_Sum", "cf_scxwo_Mean", "top10_scxwo_Sum", "top10_scxwo_Mean") #, "unit")
 #   ref<- abm_ref(table = paste0("cf_glid_",testlevel,"_frac_aggr"), unit_code=testunit)  %>% select(cols) %>% collect()
 #   calc<- abm_table3(unit_code=testunit)
@@ -59,50 +86,4 @@ test_that("cf-test", {
 #   #expect_true(testresult)
 #   expect_true(all.equal(ref_df, calc_df, check.names=FALSE, tolerance=acc_tolerance))
 # })
-
-
-# Test comparing c3-table
-#----------------------------------
-test_that("c3-test", {
   
-  cols<- c("publication_year", "p_frac_Sum", "c3_frac_Sum", "c3_frac_mean_Sum")
-  refname= paste0("c3_",testlevel,"_frac_aggr")
-  
-  comp_result<- compare_ref(cols=cols, refname=refname, calctable=abm_table2)
-  expect_true(comp_result)
-  
-})
-  
-#   cols<- c("publication_year", "p_frac_Sum", "c3_frac_Sum", "c3_frac_mean_Sum")
-#   ref<- abm_ref(table = paste0("c3_",testlevel,"_frac_aggr"), unit_code=testunit)  %>% select(cols) %>% collect()
-#   calc<- abm_table2(unit_code=testunit)
-#   #colnames(calc)<- cols # not needed with all(X == Y) 
-#   
-#   ref_df<- as.data.frame(ref)
-#   calc_df<- as.data.frame(calc)
-#   
-#   expect_true(all.equal(ref_df,calc_df, check.names=FALSE, tolerance=acc_tolerance))
-# })
-
-# Test comparing jcf-table
-#----------------------------------
-test_that("jcf-test", {
-  
-  cols<- c("jrv_label_year", "Pfrac_Sum", "jrv_mean_frac_Sum", "top20_sum_frac_Sum", "top20_mean_frac_Sum")
-  refname= paste0("jcf_glid_",testlevel,"_frac_aggr")
-  
-  comp_result<- compare_ref(cols=cols, refname=refname, calctable=abm_table4)
-  expect_true(comp_result)
-  
-})
-  
-#   cols<- c("jrv_label_year", "Pfrac_Sum", "jrv_mean_frac_Sum", "top20_sum_frac_Sum", "top20_mean_frac_Sum")
-#   ref<- abm_ref(table = paste0("jcf_glid_",testlevel,"_frac_aggr"), unit_code=testunit)  %>% select(cols) %>% collect()
-#   calc<- abm_table4(unit_code=testunit)
-#   
-#   ref_df<- as.data.frame(ref)
-#   calc_df<- as.data.frame(calc)
-#   calc_df$interval<- as.character(calc_df$interval)
-#   
-#   expect_true(all.equal(ref_df,calc_df, check.names=FALSE, tolerance=acc_tolerance))
-# })

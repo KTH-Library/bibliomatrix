b4bc7d6bf889dc239c46daddb97ca37aba616026
Markus Skyttner <mskyttner@pm.me>
2019-11-19 17:20:53 +0100

    add summary indicators to public data and fcn for private data

---
+++ tests/testthat/test-abm-private-data.R
@@ -0,0 +1,31 @@
+# Set this flag to FALSE when testing locally, leave TRUE to skip these tests on Travis
+# since Travis does not have access to closed data sources
+skip_db_tests <- TRUE
+
+
+test_that("private data can be retrieved from five different kthids", {
+  
+  library(dplyr)
+  library(purrr)
+  
+  kthids <- trimws(readr::read_lines(
+"u1kzf1xh
+u1g9umtq
+u1jr9fll 
+u1ygqmuy
+u13bp6vd
+u18qe64m"))
+
+  # if this is changed to include all entries ie kthids[1:6]
+  # the test fails
+  res <- map(kthids[1:5], abm_private_data)
+
+  has_meta <- function(x) x %>% pluck("meta") %>% nrow() > 0
+  has_unit <- function(x) x %>% pluck("units") %>% pluck(1, 1) %>% nrow() > 0
+  has_both <- function(x) all(has_meta(x), has_unit(x))
+  
+  all_has_both <- all(map_lgl(res, has_both))
+  
+  expect_true(all_has_both)
+  
+})

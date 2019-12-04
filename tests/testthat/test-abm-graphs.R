b216c0f8314adee9f4c92e6fe31618a9540d158a
agnepagne <agne.larsson@gmail.com>
2019-12-04 09:58:19 +0100

    change pt_order -> pubtype_order in test

--- tests/testthat/test-abm-graphs.R
+++ tests/testthat/test-abm-graphs.R
@@ -2,7 +2,7 @@ test_that("abm_public_kth contains sort order table for publication types", {
 
   # the lookup table exists and has more than 10 rows  
   is_included <- 
-    abm_public_kth$pt_ordning %>% nrow() > 10
+    abm_public_kth$pubtype_order %>% nrow() > 10
   
   expect_true(is_included)
   

08ab7cee18245be70c1604d4d6fea3f5092ab6d0
Markus Skyttner <mskyttner@pm.me>
2019-12-16 15:09:09 +0100

    update app, clean up and removal of global variable

--- inst/shiny-apps/abm/server.R
+++ inst/shiny-apps/abm/server.R
@@ -25,7 +25,6 @@ server <- function(input, output, session) {
     kthid <- function() {
         # if shinyproxy with saml then we get user identity as kthid@kth.se
         # if shinyproxy with ldap then we get user identity as LDAP accountname
-        if (input$use_prerendered) return (177)
         if (is_saml(ua)) {
             kthid <- parse_id(ua)
         } else {
@@ -49,95 +48,50 @@ server <- function(input, output, session) {
             multiple = FALSE, selectize = FALSE, width = "100%")
     })
     
-    # using the API, which could be anywhere or external
-
-    observe({
-        req(input$unitid)
-        # if the unit id not is for an org then use employee endpoint
-        API <- ifelse(!input$unitid %in% abm_public_kth$meta$Diva_org_id, 
-            ABM_API_EMP, ABM_API_UNIT)
-        # TODO: this probably will affect other users sessions and should be changed
-        dash_src <<- sprintf(API, input$unitid)
-        if (!input$use_prerendered)
-            cat(dash_src, "\n")
-    })
+    dash_src <- function(id) {
+        if (id == kthid())
+            return (sprintf(ABM_API_EMP, id))
+        if (id %in% abm_public_kth$meta$Diva_org_id)
+            return (sprintf(ABM_API_UNIT, id))
+    }
+    
+    is_employee <- function(id) id == kthid()
     
     output$frame <- renderUI({
         
         req(input$unitid)
+        loc_www <- system.file("shiny-apps", "abm", "www", package = "bibliomatrix")
         
         if (input$use_prerendered == TRUE) {
-            f <- file.path(prerender_cache_location(), 
-                paste0(input$unitid, ".html"))
-            # txt <- read_lines(f)
-            # s1 <- "<div class=\"navbar navbar-inverse navbar-fixed-top\" role=\"navigation\">"
-            # s2 <- "<div class=\"navbar navbar-inverse\" role=\"navigation\">"
-            # txt <- str_replace(txt, s1, s2)
-            # write_lines(txt, f)
-#            f2 <- filter_fragment(f, search = "", replace = "")
-            # issue https://community.rstudio.com/t/generating-markdown-reports-from-shiny/8676/5
-            # includeHTML(f)
-            # active_dash <- file.path(system.file("shiny-apps", "abm", "www", package = "bibliomatrix"),
-            #     "activedash.html")
-            # cat("copying ", f, " to ", active_dash)
-            # res <- file.copy(f, "activedash.html", overwrite = TRUE)
-            # cat("outcome: res")
-            # embed_data <- function(path)
-            #     paste0("data:", mime::guess_type(path), ";base64,", 
-            #            base64enc::base64encode(path))
-            tags$iframe(src = paste0(input$unitid, ".html"), width = "100%", height = "100%",
-                        frameborder = 0, scrolling = 'auto')
-        } else {
-            f <- paste0(openssl::sha1(kthid()), ".html")
-            if (!file.exists(f)) {
-                cat("Embedding from: ", dash_src, "\n")
-                w <- httr::GET(dash_src)
-                if (httr::status_code(w) == 200) {
+            f <- paste0(input$unitid, ".html")
+            ff <- file.path(loc_www, f)
+            if (!file.exists(ff))
+                cat("Couldn't find cached data for ", f, "\n")
+        } else if (input$use_prerendered == FALSE) {
+            f <- paste0(input$unitid, ".html")
+            if (is_employee(input$unitid))
+                f <- paste0(openssl::sha1(input$unitid), ".html")
+            ff <- file.path(loc_www, f)
+            if (!file.exists(ff)) {
+                www <- dash_src(input$unitid)
+                cat("Not in cache, requesting from: ", www, "\n")
+                w <- try(httr::GET(www), silent = TRUE)
+                if (!inherits(w, 'try-error') && httr::status_code(w) == 200) {
                     html <- httr::content(w, as = "raw")
-                    loc_www <- system.file("shiny-apps", "abm", "www", package = "bibliomatrix")
-                    writeBin(html, file.path(loc_www, f))
+                    writeBin(html, f)
                 } else {
-                    msg <- sprintf("<pre>Error from API at %s</pre>", dash_src)
+                    msg <- charToRaw(sprintf("<h1>API did not return data</h1>"))
                     f <- sprintf("data:text/html;base64,%s", base64enc::base64encode(msg))
                 }
+            } else {
+                cat("Embedding from cache: ", f, "\n")
             }
-            #data_uri <- sprintf("data:text/html;base64,%s", base64enc::base64encode(html))
-            tags$iframe(src = f, width = "100%", height = "100%",
-                frameborder = 0, scrolling = 'auto')
+
         }
-        
+        tags$iframe(src = f, width = "100%", height = "100%",
+                    frameborder = 0, scrolling = 'auto')
     })    
     
-    # this could be used to render from within the package
-    
-    output$dash <- renderUI({
-        
-        req(input$unitid)
-        
-        if (input$unitid %in% abm_public_kth$meta$Diva_org_id) {
-            uc <- 
-                abm_public_kth$meta %>% 
-                filter(Diva_org_id == as.integer(input$unitid)) %>% 
-                pull(unit_code)
-           
-
-            report <- system.file("extdata", "abm.Rmd", package = "bibliomatrix")
-            f <- rmarkdown::render(report, params = list(unit_code = uc,
-                is_employee = FALSE, embed_data = FALSE, use_package_data = TRUE))
-        } else {
-            report <- system.file("extdata", "abm.Rmd", package = "bibliomatrix")
-            f <- rmarkdown::render(report, params = list(unit_code = input$unitid,
-                is_employee = TRUE, embed_data = TRUE, use_package_data = FALSE))
-        }
-        includeHTML(f)
-        #readBin(f, "raw", n = file.info(f)$size)
-    })
-    
-    # TODO: think about caching content... to www? and use...
-    # tags$iframe(src = './myMarkdown.html', # put myMarkdown.html to /www
-    #             width = '100%', height = '800px', 
-    #             frameborder = 0, scrolling = 'auto'
-    # )
     
 }
 

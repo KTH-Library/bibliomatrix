b37f0d5f15861875db1c7f376fa860ddde31266e
Markus Skyttner <mskyttner@pm.me>
2020-05-14 15:40:24 +0200

    add theming in abm_bullet_plotly fcn

--- R/graphs-plotly.R
+++ R/graphs-plotly.R
@@ -13,19 +13,19 @@ abm_bullet_plotly <- function(label, value, reference, roundto = 1, pct = FALSE)
   
   p1 <- 
     abm_bullet(label, value, reference, roundto, pct) +
-    theme_kth() + 
-    # override some theme settings
-    theme(
-      plot.title=element_text(size = 12),
-      axis.text.x=element_text(size = 8),
-      axis.title.x=element_blank(),
-      axis.title.y=element_blank(),
-      axis.line.y=element_blank(),
-      axis.line.x=element_blank(),
-      axis.text.y=element_blank(),
-      axis.ticks.x=element_blank(),
-      panel.grid.major=element_blank(),
-      panel.grid.minor=element_blank()
+    ktheme::theme_kth_osc(plot_title_face = "plain", plot_title_size = 14) + 
+    ggplot2::theme(plot.margin = margin(0, 0, 10, 0)) +
+    ggplot2::theme(
+      axis.title.x = element_blank(),
+      axis.line.y = element_blank(),
+      axis.text.y = element_blank(),
+      axis.ticks.y = element_blank(),
+      axis.title.y = element_blank(),
+      #      legend.position = "none",
+      #      panel.background = element_blank(),
+      #      panel.border = element_blank(),
+      panel.grid.major.y = element_blank(),
+      panel.grid.minor = element_blank()
     ) +
     labs(title = "")
   

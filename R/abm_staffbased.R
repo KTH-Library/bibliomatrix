5ea0a27fb8296994c84f831d271ed8239009dae7
Markus Skyttner <mskyttner@pm.me>
2020-09-01 10:46:24 +0200

    fix roxygen2 annotation for unit_staff fcn and add test

--- R/abm_staffbased.R
+++ R/abm_staffbased.R
@@ -16,18 +16,21 @@
 #' Get members of unit based on slug
 #' 
 #' @param unit_slug string representing KTH unit
+#' @importFrom kthapi kth_catalog
 #' @return tibble of unit numbers, including kthid
 #' @export
 
-unit_staff <- function(unit_slug = NULL){
-  org_users<- kth_catalog(slug = unit_slug)$users
-  org_users$kthid<- ""
-  nr_users<- dim(org_users)[1]  
+unit_staff <- function(unit_slug = NULL) {
   
-  for(i in 1:nr_users){
-    id<- kth_profile(username = org_users$username[i])$content$kthId
-    org_users$kthid[i]<- id
+  org_users <- kth_catalog(slug = unit_slug)$users
+  org_users$kthid <- ""
+  nr_users <- dim(org_users)[1]
+  
+  for (i in 1:nr_users) {
+    id <- kth_profile(username = org_users$username[i])$content$kthId
+    org_users$kthid[i] <- id
   }
+  
   org_users
 }
 
@@ -38,6 +41,7 @@ unit_staff <- function(unit_slug = NULL){
 #' @return tibble with all staff-based ABM data for selected organizational unit
 #' @export
 abm_staff_data <- function(con = con_bib(), kthids) {
+  
     res <- con %>%
       tbl("masterfile") %>%
       filter(Unit_code %in% kthids)  %>% 
@@ -45,11 +49,7 @@ abm_staff_data <- function(con = con_bib(), kthids) {
     
     auth_count<- res %>% group_by(PID) %>% tally() 
     
-    res<- res %>% inner_join(auth_count, by="PID") %>% distinct(PID, WebofScience_ID, .keep_all = TRUE)
-    return(res)
-    
+    res %>% 
+      inner_join(auth_count, by="PID") %>% 
+      distinct(PID, WebofScience_ID, .keep_all = TRUE)
 }
-
-#' Dummy to test git
-#' 
-#' Future function

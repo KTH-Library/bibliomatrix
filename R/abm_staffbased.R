00e2c8c75285b1cce35f219e325c86a5906bdb39
tobias <tjep@kth.se>
2020-09-04 15:52:34 +0200

    Modified version of abm_table1 created as well as dummy flexdashboard for staff-based results

--- R/abm_staffbased.R
+++ R/abm_staffbased.R
@@ -34,7 +34,7 @@ unit_staff <- function(unit_slug = NULL) {
   org_users
 }
 
-#' Get members of unit based on slug
+#' Get publications from members of unit via kthids
 #' 
 #' @param con A database connection
 #' @param kthids a list of KTH-ids to retrieve publications for
@@ -52,4 +52,54 @@ abm_staff_data <- function(con = con_bib(), kthids) {
     res %>% 
       inner_join(auth_count, by="PID") %>% 
       distinct(PID, WebofScience_ID, .keep_all = TRUE)
+}
+
+
+# Alt versions of abm_table functions
+#---------------------------------------
+#' Retrieve Table 1 (Publications in DiVA) for ABM
+#' 
+#' @param con connection to db, default is to use mssql connection
+#' @param data dataset with publications as tibble
+#' @param analysis_start first publication year of analysis, default 2012
+#' @param analysis_stop last publication year of analysis, default 2018
+#' @return data frame with publications by type and year
+#' @import DBI dplyr tidyr purrr
+#' @importFrom stats weighted.mean
+#' @export
+abm_table1_alt <- function(con = con_bib(), data, analysis_start = abm_config()$start_year, analysis_stop = abm_config()$stop_year){
+  
+  #attach(data)
+  # Get publication level data for selected unit
+  orgdata <- data %>%
+    filter(Publication_Year >= analysis_start &
+             Publication_Year <= analysis_stop) %>%
+    mutate(wos_bin = ifelse(!is.na(Doc_id),1,0))
+  
+  # Year dependent part of table
+  table1 <-
+    orgdata %>%
+    group_by(Publication_Year, Publication_Type_DiVA) %>%
+    summarise(P_frac = sum(Unit_Fraction, na.rm = TRUE)) %>%
+    collect() %>%
+    arrange(Publication_Year) %>%
+    pivot_wider(names_from = Publication_Year, values_from = P_frac) %>%
+    ungroup()
+  
+  # Summary part of table
+  table2 <-
+    orgdata %>%
+    group_by(Publication_Type_DiVA) %>%
+    summarise(P_frac = sum(Unit_Fraction, na.rm = TRUE),
+              WoS_coverage = sum(wos_bin * Unit_Fraction, na.rm = TRUE) / sum(Unit_Fraction, na.rm = TRUE)) %>%
+    ungroup() %>% 
+    collect()
+  
+  pubtype_order <- get_pubtype_order(con)
+  
+  table1 %>%
+    inner_join(table2, by = "Publication_Type_DiVA") %>%
+    inner_join(pubtype_order, by = c("Publication_Type_DiVA" = "diva_publication_type")) %>%
+    arrange(pt_ordning) %>%
+    select(-pt_ordning)
 }

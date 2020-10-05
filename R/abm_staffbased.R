49b43a1adb3d7bd166ece52d491d5b8b1e2e3f94
tobias <tjep@kth.se>
2020-10-05 22:24:46 +0200

    Full check and vignettes build for Travis check

--- R/abm_staffbased.R
+++ R/abm_staffbased.R
@@ -143,8 +143,8 @@ abm_table2_alt <- function(con = con_bib(), data, analysis_start = abm_config()$
     orgdata %>%
     group_by(Publication_Year) %>%
     summarise(P_frac = sum(Unit_Fraction, na.rm = TRUE),
-              C3_frac = sum(Unit_Fraction * Citations_3yr, na.rm = TRUE),
-              C3 = sum(Unit_Fraction * Citations_3yr, na.rm = TRUE) / sum(Unit_Fraction, na.rm = TRUE),
+              C3 = sum(Unit_Fraction * Citations_3yr, na.rm = TRUE),
+              C3_frac = sum(Unit_Fraction * Citations_3yr, na.rm = TRUE) / sum(Unit_Fraction, na.rm = TRUE),
               P_uncited = sum(Unit_Fraction * uncited, na.rm = TRUE),
               Share_uncited = sum(Unit_Fraction * uncited, na.rm = TRUE) / sum(Unit_Fraction, na.rm = TRUE)) %>%
     ungroup() %>%
@@ -170,6 +170,61 @@ abm_table2_alt <- function(con = con_bib(), data, analysis_start = abm_config()$
   
   bind_rows(table1, table2)
   
+}
+
+
+#' Retrieve Table 3 (Field normalized citations) for ABM
+#' 
+#' @param con connection to db, default is to use mssql connection
+#' @param data dataset with publications as tibble
+#' @param analysis_start first publication year of analysis, default 2012
+#' @param analysis_stop last publication year of analysis, default 2018
+#' @return tibble with field normalized citations and number/share of top10 publications by 3 year interval
+#' @import DBI dplyr tidyr purrr
+#' @importFrom stats weighted.mean
+#' @export
+
+abm_table3_alt <- function(con = con_bib(), data, analysis_start = abm_config()$start_year, analysis_stop = abm_config()$stop_year){
+  
+  # Get publication level data for selected unit, relevant WoS doctypes only
+  orgdata <- data %>%
+    filter(Publication_Year >= analysis_start &
+             Publication_Year <= analysis_stop - 1 &
+             Publication_Type_WoS %in% c("Article", "Review") & 
+             !is.na(cf))
+  
+  # Duplicate rows so that publications are connected to all intervals they should belong to according to publication year
+  orgdata3year <-
+    orgdata %>%
+    collect() %>%
+    inner_join(sliding_intervals(analysis_start, analysis_stop - 1, 3),
+               by = c("Publication_Year" = "x"))
+  
+  # Year dependent part of table
+  table1 <-
+    orgdata3year %>%
+    group_by(interval) %>%
+    summarise(P_frac = sum(Unit_Fraction_adj, na.rm = TRUE),
+              cf = weighted.mean(cf, Unit_Fraction_adj, na.rm = TRUE),
+              top10_count = sum(Ptop10*Unit_Fraction_adj, na.rm = TRUE),
+              top10_share = weighted.mean(Ptop10, Unit_Fraction_adj, na.rm = TRUE)) %>%
+    ungroup()
+  
+  # No summary row if no data
+  if(nrow(table1) == 0)
+    return(table1)
+  
+  # Summary part of table
+  table2 <-
+    orgdata %>%
+    summarise(P_frac = sum(Unit_Fraction_adj, na.rm = TRUE),
+              cf = sum(cf * Unit_Fraction_adj, na.rm = TRUE) / sum(Unit_Fraction_adj, na.rm = TRUE),
+              top10_count = sum(Ptop10 * Unit_Fraction_adj, na.rm = TRUE),
+              top10_share = sum(Ptop10 * Unit_Fraction_adj, na.rm = TRUE) / sum(Unit_Fraction_adj, na.rm = TRUE)) %>%
+    collect() %>% 
+    mutate(interval = "Total")
+  
+  bind_rows(table1, table2)
 }
 
 #' Retrieve WoS coverage for peer reviewed DiVA publication types

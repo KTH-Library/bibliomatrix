5528e37b7636f22b5e681d164c528d1ed93a13b4
Markus Skyttner <mskyttner@pm.me>
2021-02-21 21:56:53 +0100

    add remote sync of authorbased tables

--- data-raw/public_data.R
+++ data-raw/public_data.R
@@ -13,7 +13,7 @@ usethis::use_data(abm_public_kth, internal = FALSE, overwrite = TRUE)
 # persist crawl data locally (researchers, divisions, unit_stats)
 db_upload_crawl()
 
-# TODO: sync/upload to remote db too
-db_sync_table("unit_stats", con_src = con_bib_sqlite(), con_dest = con_bib_mssql())
-db_sync_table("researchers", con_src = con_bib_sqlite(), con_dest = con_bib_mssql())
-db_sync_table("divisions", con_src = con_bib_sqlite(), con_dest = con_bib_mssql())
+# also sync this data remotely
+db_sync_table("unit_stats", con_src = con_bib_sqlite(), con_dest = con_bib_mssql(), overwrite = TRUE)
+db_sync_table("researchers", con_src = con_bib_sqlite(), con_dest = con_bib_mssql(), overwrite = TRUE)
+db_sync_table("divisions", con_src = con_bib_sqlite(), con_dest = con_bib_mssql(), overwrite = TRUE)

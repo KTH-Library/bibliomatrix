11886aa3b725b5d8c6a463b50f4df08ecd4c2b56
fileunderwater <file_under_water@yahoo.se>
2025-11-10 13:15:19 +0100

    typo on fontname?

--- R/ui-elements.R
+++ R/ui-elements.R
@@ -1710,5 +1710,5 @@ abm_plotlyfy <- function(graphobj, tooltip, optionsbar = FALSE, showlegend = TRU
   graphobj |> 
   ggplotly(tooltip = tooltip) |>  
   config(displayModeBar = optionsbar) |>
-  layout(autosize = TRUE, showlegend = showlegend, font = list(family = "figtree"))
+  layout(autosize = TRUE, showlegend = showlegend, font = list(family = "Figtree"))
 }
